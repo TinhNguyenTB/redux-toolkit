301d3c5bb4e2b82c896193a1ebc0dcb65de31acb
TinhNguyenTB <tinh271103@gmail.com>
2024-01-01 20:39:47 +0700

    fetch data without redux

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './redux/slices/counterSlice';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
   const dispatch = useDispatch();
   const count = useSelector((state) => state.counter.value);
 
+  const [listUsers, setListusers] = useState([]);
+
+  useEffect(() => {
+    fetchAllUsers();
+  }, [])
+
+  const fetchAllUsers = async () => {
+    let res = await axios.get("http://localhost:8080/users/all");
+    setListusers(res.data ? res.data : [])
+    console.log(res.data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
+        {/* <h1>Hello world</h1>
         <div className='btn-actions'>
           <button
             onClick={() => dispatch(increment())}
@@ -22,7 +36,29 @@ function App() {
         </div>
 
         <br></br>
-        <div>count = {count}</div>
+        <div>count = {count}</div> */}
+        <div>
+          <table>
+            <thead>
+              <th>ID</th>
+              <th>Email</th>
+              <th>Username</th>
+            </thead>
+            <tbody>
+              {listUsers && listUsers.length > 0 &&
+                listUsers.map((item, index) => {
+                  return (
+                    <tr>
+                      <td>{item.id}</td>
+                      <td>{item.email}</td>
+                      <td>{item.username}</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
       </header>
     </div>
   );
